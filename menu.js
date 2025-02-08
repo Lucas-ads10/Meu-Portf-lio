2bb29589077a868ca5269cbf0324158f6f6d2de7
Lucas-ads10 <lucasdssilva13@hotmail.com>
2025-02-08 18:27:14 -0300

    Add menu lateral e Java script

---
+++ menu.js
@@ -0,0 +1,30 @@
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+// Adiciona um comportamento de rolagem suave com ajuste de offset, caso tenha um cabeçalho fixo
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        window.scrollTo({
+            top: targetElement.offsetTop - 50, // Ajuste o valor '50' para a altura do seu cabeçalho fixo
+            behavior: 'smooth'
+        });
+    });
+});
